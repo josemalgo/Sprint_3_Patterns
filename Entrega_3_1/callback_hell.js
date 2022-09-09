090c28c32a39d3eeb51fdc663e3127a91457241e
jose1432@gmail.com <jose1432@gmail.com>
2022-09-09 22:20:02 +0200

    callback hell complete

--- Entrega_3_1/callback_hell.js
+++ Entrega_3_1/callback_hell.js
@@ -18,14 +18,24 @@ const reverseText = str =>
 
 // Read and reverse contents of text files in a directory
 readdir(inbox, (error, files) => {
+  console.log(inbox);
+  getFilesDirectories(error, files);
+} );
+
+
+function getFilesDirectories(error, files) {
   if (error) return console.log("Error: Folder inaccessible");
   files.forEach(file => {
     readFile(join(inbox, file), "utf8", (error, data) => {
-      if (error) return console.log("Error: File error");
-      writeFile(join(outbox, file), reverseText(data), error => {
-        if (error) return console.log("Error: File could not be saved!");
-        console.log(`${file} was successfully saved in the outbox!`);
-      });
+      writeReverseFile(file, error, data);
     });
   });
-});
+}
+
+function writeReverseFile(file, error, data){
+  if (error) return console.log("Error: File error");
+  writeFile(join(outbox, file), reverseText(data), error => {
+    if (error) return console.log("Error: File could not be saved!");
+    console.log(`${file} was successfully saved in the outbox!`);
+  });
+}
